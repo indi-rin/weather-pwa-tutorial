63182e5e359da9dd2d2cdd746ca0afb1f2e192dc
Indi Rinearson <indi.rinearson@gmail.com>
2022-01-14 13:12:08 -0800

    api call sets weather data in state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { fetchWeather } from "./api/fetchWeather";
 import "./App.css";
 
 const App = () => {
-  return <h1>App</h1>;
+  const [query, setQuery] = useState(""); // user input for what city
+  const [weather, setWeather] = useState({});
+
+  const search = async (event) => {
+    if (event.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data); // update weather in state to hold info from API call
+      setQuery(""); // reset query upon submission
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyPress={search}
+      />
+    </div>
+  );
 };
 
 export default App;
